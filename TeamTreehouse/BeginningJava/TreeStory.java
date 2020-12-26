a1cafed313e3a00518af403264d0d9c5a94c4dd7
Myron <chbymnky@gmail.com>
2020-12-25 22:07:54 -0500

    creating a new fill in the blank sentence

--- TeamTreehouse/BeginningJava/TreeStory.java
+++ TeamTreehouse/BeginningJava/TreeStory.java
@@ -14,7 +14,16 @@ public class TreeStory {
     String adjective = console.readLine("Enter an adjective:  ");
 
     // printing with multiple variables
-    console.printf("%s is very %s.", name, adjective);
+    console.printf("%s is very %s.\n", name, adjective);
+
+    // __Name__ is a __adjective__ __noun__. They are always __adverb__ __verb(ing)__.
+    String yourName = console.readLine("Enter your name:  ");
+    String nameAdjective = console.readLine("Enter an adjective:  ");
+    String nameNoun = console.readLine("Enter a noun:  ");
+    String adverb = console.readLine("Enter an adverb: ");
+    String verbing = console.readLine("Enter a verb ending in (ing):  ");
+
+    console.printf("%s is a %s %s. They are always %s %s.\n", yourName, nameAdjective, nameNoun, adverb, verbing);
   }
 
 }
