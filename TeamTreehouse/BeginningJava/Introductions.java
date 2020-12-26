6cbf7eada2ed68919de49c7e9c18cc4ce6cfbb69
Myron <chbymnky@gmail.com>
2020-12-25 20:53:34 -0500

    updating code from tutorial for Beginning Java

--- TeamTreehouse/BeginningJava/Introductions.java
+++ TeamTreehouse/BeginningJava/Introductions.java
@@ -5,6 +5,13 @@ public class Introductions {
   public static void main(String[] args) {
     Console console = System.console();
     // Welcome to the Introductions program!  Your code goes below here
-    console.printf("Hello, my name is Myron");
-  }
+
+    // Variable declaration
+    // Java is trongly typed so we have to define the type of the variabe
+    String firstName = "Myron";
+
+    // format string uses %s as a placeholder in the .printf method
+    console.printf("Hello, my name is %s\n", firstName);
+    console.printf("%s is learning how to write Java\n", firstName);
+}
 }
